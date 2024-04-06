91a35bb3d527a315e5ec6ef197e9375c5e768cfb
hasanasadov <125257868+hasanasadov@users.noreply.github.com>
2024-04-07 02:52:15 +0400

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,21 +1,11 @@
-
 #!/usr/bin/env python3
-""" Async comprenhesion gen """
-import asyncio
+''' Description: Import async_generator from the previous task and then write
+                 a coroutine called async_comprehension that takes no arguments
+'''
 from typing import List
-
 
 async_generator = __import__('0-async_generator').async_generator
 
-
 async def async_comprehension() -> List[float]:
-    """
-        Generate numbers with async comprenhension
-
-        Args:
-            void
-
-        Return:
-            float random numbers
-    """
-    return ([i async for i in async_generator()])
+    ''' Return list of values yielded by async_generator. '''
+    return [value async for value in async_generator()]
