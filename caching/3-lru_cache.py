e390087a49e20e69c1f9579f4086a6f9f313af53
hasanasadov <125257868+hasanasadov@users.noreply.github.com>
2024-04-07 02:54:35 +0400

    Update 3-lru_cache.py

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,48 +1,36 @@
-
 #!/usr/bin/python3
-''' LRU Caching: Create a class LRUCache that inherits from BaseCaching
-                 and is a caching system
-'''
+''' self descriptive code '''
+
+from collections import OrderedDict
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    ''' An LRU Cache.
-        Inherits all behaviors from BaseCaching except, upon any attempt to
-        add an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS), it discards the least recently used entry to
-        accommodate for the new one.
-        Attributes:
-          __init__ - method that initializes class instance
-          put - method that adds a key/value pair to cache
-          get - method that retrieves a key/value pair from cache '''
+    ''' self descriptive '''
 
     def __init__(self):
-        ''' Initialize class instance. '''
         super().__init__()
-        self.keys = []
+        self.lru_order = OrderedDict()
 
     def put(self, key, item):
-        ''' Add key/value pair to cache data.
-            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
-            discard least recently used entry in cache to accommodate new
-            entry. '''
-        if key is not None and item is not None:
+        ''' self descriptive '''
+        if key and item:
+            self.lru_order[key] = item
+            self.lru_order.move_to_end(key)
             self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
-            else:
-                self.keys.append(self.keys.pop(self.keys.index(key)))
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discard = self.keys.pop(0)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            item_discarded = next(iter(self.lru_order))
+            del self.cache_data[item_discarded]
+            print("DISCARD:", item_discarded)
+
+        if len(self.lru_order) > BaseCaching.MAX_ITEMS:
+            self.lru_order.popitem(last=False)
 
     def get(self, key):
-        ''' Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None. '''
-        if key is not None and key in self.cache_data:
-            self.keys.append(self.keys.pop(self.keys.index(key)))
+        ''' self descriptive '''
+        if key in self.cache_data:
+            self.lru_order.move_to_end(key)
             return self.cache_data[key]
         return None
