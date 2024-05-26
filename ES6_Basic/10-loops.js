1396f3e1be441d4f7a446a28d1e3137e681f9fd7
hasanasadov <125257868+hasanasadov@users.noreply.github.com>
2024-05-26 14:16:31 +0400

    Delete ES6_Basic directory

--- ES6_Basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const final = [];
-  for (const value of array) {
-    final.push(appendString + value);
-  }
-
-  return final;
-}
